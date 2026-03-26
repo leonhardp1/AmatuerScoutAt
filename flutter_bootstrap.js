4563831e90772f9918b80e694475fd370da78aa9
leonhardp1 <leonhard.perstling@gmail.com>
2026-03-26 10:41:07 +0100

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1330768856"
+    serviceWorkerVersion: "4039831020"
   }
 });
